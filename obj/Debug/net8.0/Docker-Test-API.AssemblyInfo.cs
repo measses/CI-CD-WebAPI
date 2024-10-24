f1ce298ca7ec48c9d87976fb27cf568ca2506a37
Muhammed Mert Araz <67739721+measses@users.noreply.github.com>
2024-10-24 21:48:50 +0300

    dockerfile created

--- obj/Debug/net8.0/Docker-Test-API.AssemblyInfo.cs
+++ obj/Debug/net8.0/Docker-Test-API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Docker-Test-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aebe39ed3a61b3720a9b4e4eeb9a145e8c3454bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Docker-Test-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Docker-Test-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
